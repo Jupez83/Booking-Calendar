a5295bdf5778fa3c8b6c62a0ecab532749d61b4a
Jupez83 <jussi.ahtiainen83@gmail.com>
2016-01-24 13:09:20 +0200

    mainController handles navigation between calendars.

--- app/mainController.js
+++ app/mainController.js
@@ -1,6 +1,7 @@
-module.exports = function($rootScope, $scope, $state, authService) {
+module.exports = function($rootScope, $scope, $state, $stateParams, $q, authService, calendarService) {
   $rootScope.loginData = {};
-  
+  $scope.calendars = [];
+
   authService.isLoggedIn().then(function(data) {
     $rootScope.loginData.loggedIn = data.authoricated;
 
@@ -10,7 +11,45 @@ module.exports = function($rootScope, $scope, $state, authService) {
     }
   });
 
+  $scope.makeLogin = function() {
+    updateCalendarList().then(function(data) {
+      if (_.isEmpty($scope.calendars)) {
+        $state.go('newCalendar');
+      } else {
+        $state.go('calendar', {calendarId: $scope.calendars[0]._id});
+      }
+    });
+  };
+
   $scope.isActive = function(page) {
     return page === $state.current.name;
   };
+
+  function updateCalendarList() {
+    var deferred = $q.defer();
+
+    calendarService.getAllCalendars().then(function(data) {
+      if (_.has(data, 'data')) {
+        deferred.resolve();
+        $scope.calendars = data.data;
+      } else {
+        deferred.reject();
+      }
+    });
+
+    return deferred.promise;
+  }
+
+  $scope.$on('calendarListChanged', function() {
+    console.log('calendarListChanged');
+    updateCalendarList().then(function() {
+      if (_.isEmpty($scope.calendars)) {
+        $state.go('newCalendar');
+      } else {
+        if (!_.some($scope.calendars, ['_id', $stateParams.calendarId])) {
+          $state.go('calendar', {calendarId: $scope.calendars[0]._id});
+        }
+      }
+    });
+  });
 };
