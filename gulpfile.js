ce6a5e856d2d3304299fe15f68142c90662e0333
Jupez83 <jussi.ahtiainen83@gmail.com>
2016-02-07 13:12:13 +0200

    Updated gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,32 @@
-var gulp = require('gulp');
 var browserify = require('browserify');
+var del = require('del');
+var gulp = require('gulp');
+var ngAnnotate = require('gulp-ng-annotate');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
-var ngAnnotate = require('gulp-ng-annotate');
-var uglify = require('gulp-uglify');
 
-gulp.task('browserify', function() {
+gulp.task('scripts', function() {
   return browserify('./app/app.js')
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(buffer())
+    .pipe(gulp.dest('app/dist'))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(gulp.dest('app/dist'));
 });
 
-gulp.task('test', function() {
-  return browserify('./app/app.js')
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('app/dist'));
+gulp.task('clean', function() {
+  return del(['app/dist/*.*']);
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('default', ['clean'], function() {
+  gulp.start('scripts');
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['app/**/*.js', '!app/dist/**/*.js'], ['scripts']);
+});
